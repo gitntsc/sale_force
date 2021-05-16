cf3f4e65395c188cb7c0f7d08628e58417999f52
mosjikung <mosjikung@gmail.com>
2021-05-16 23:14:44 +0700

    new commit

--- testpage2.php
+++ testpage2.php
@@ -1,31 +1,6 @@
-
- <div class="form-group">
-                            <label>บริษัท</label>
-                            <?php
-                           $strSQL11 = "SELECT * from customer where section = '".$_SESSION['section']."'";
-                            $objQuery11 = mysqli_query($objCon,$strSQL11);
-                            
-                            ?>
-                            <select class="form-control" id="company" name="company" onchange="myfunction()">
-                            <?php
-                            while ($objResult11 = mysqli_fetch_array($objQuery11)){
-                            ?>
-                            <option value="<?php echo $objResult11['name_company'];?>"><?php echo $objResult11['name_company'];?></option>
-		
-
-                            <?php
-                            }
-                            ?>
-                            </select>
-                              
-                        </div>
 <script>
-
-function myfunction(){
-var x = document.getElementById("company").value;
-<?php
-include "connect.php";
-$strSQL = "SELECT * FROM where name_company = "
-?>
+function x(){
+    document.write(2+5+"8");
 }
+x();
 </script>
